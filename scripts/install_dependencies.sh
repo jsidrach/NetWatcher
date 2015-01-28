cd19d5a43a7d152ca4b3983bfc7fbd070311ba49
JSidrach <juan.sidrach@gmail.com>
2015-01-28 10:26:11 +0100

    Added installation of mod_rewrite

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # Intall the required packages
 sudo apt-get install php5-common libapache2-mod-php5 php5-cli php5-xsl GraphViz curl gettext poedit git
+# Active mod_rewrite
+sudo a2enmod rewrite
+# Restart apache
+sudo service apache2 restart
+# If .htaccess is not being used, check grep /etc/apache2/apache2.conf | AllowOverride, and change the conf of the html dir to AllowOverride All
