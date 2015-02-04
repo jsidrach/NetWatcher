f0e82e1b06f372424222c5ce3b4535c7f957e702
JSidrach <juan.sidrach@gmail.com>
2015-02-04 11:57:38 +0100

    Structured script names and folders

--- fpga_server/routes/common.js
+++ fpga_server/routes/common.js
@@ -1,9 +1,11 @@
+// Common functions for the scripts
+
 // Package dependencies
-var fs       = require('fs');
-var path     = require('path'); 
+var fs         = require('fs');
+var path       = require('path');
 
 // Reads a json in the messages folder
-var readJSON = function (file, obj, callback)
+exports.readJSON = function (file, obj, callback)
 {
   fs.readFile(path.resolve(__dirname, '../messages/', file + '.json'), 'utf8', function (err, data) {
     if(err)
@@ -15,15 +17,5 @@ var readJSON = function (file, obj, callback)
       obj = JSON.parse(data);
     }
     callback(obj);
-  });
-};
-
-
-// test example to see if everything is working and the json reader is ok
-exports.example = function(req, res) {
-  var ans;
-  readJSON('example', ans, function(ans)
-  {
-    res.json(ans);
   });
 };
