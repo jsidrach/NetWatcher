f218c6d312fdd61b3883e4558122079de861cfa8
JSidrach <juan.sidrach@gmail.com>
2015-02-06 12:22:02 +0100

    Migrated to io.js

---
+++ fpga_server/scripts/install_driver.sh
@@ -0,0 +1,12 @@
+#!/bin/sh - 
+
+# Remove the previous driver
+rmmod nfp_driver.ko
+
+# Instantiate the driver
+insmod ./bin/nfp_driver.ko
+chmod 777 /dev/nfp
+
+# Mount with HugePages
+mkdir -p /mnt/huge
+mount -t hugetlbfs none /mnt/huge
