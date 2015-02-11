5e10b4c9b1e7140400018c3d91586212db57ef21
JSidrach <juan.sidrach@gmail.com>
2015-02-11 10:40:52 +0100

    Add startup service of the fpga_api

--- fpga_server/scripts/install_service.sh
+++ fpga_server/scripts/install_service.sh
@@ -12,6 +12,10 @@ fi
 
 FOLDER_EXP='s|dir=\".*\"|dir=\"'${1}'\"|g'
 SERVER_EXP='s|user=\".*\"|user=\"'${2}'\"|g'
-sed -i -e ${FOLDER_EXP} -e ${SERVER_EXP} ./scripts/fpga_api &&
+sed -i -e ${FOLDER_EXP} -e ${SERVER_EXP} ./scripts/fpga_api
+# Install the service
 sudo cp -f ./scripts/fpga_api /etc/init.d
+# Set the service as a default on startup
+sudo update-rc.d fpga_api defaults
+# Restart the service
 sudo service fpga_api restart
