f85b469cef09f07fa02e830288fdda2a1c85a6a2
JSidrach <juan.sidrach@gmail.com>
2015-02-13 16:02:03 +0100

    Get all captures (draft)

--- fpga_server/scripts/do_chmod.sh
+++ fpga_server/scripts/do_chmod.sh
@@ -3,5 +3,6 @@
 sudo find . -type d -exec sudo chmod 755 {} \;
 sudo find . -type f -exec sudo chmod 644 {} \;
 sudo chmod +x scripts/*
+sudo chmod +x commands/*
 sudo chmod +x -R iojs/*
 sudo chmod +x bin/*
