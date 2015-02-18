0239db3410101d545b9ea9d158043ab27db4447a
JSidrach <juan.sidrach@gmail.com>
2015-02-18 10:55:03 +0100

    First draft of the /status call

--- fpga_api/routes/statistics.js
+++ fpga_api/routes/statistics.js
@@ -15,10 +15,10 @@ exports.ping = function (req, res) {
 // Status of the FPGA
 exports.status = function (req, res) {
   // Finite State Machine. Every transition to a new status (state) is checked with a callback test function
-  nextCallback(res, [hugePagesOn, initializedFPGA, lastTest]);
+  nextCallback(res, [hugePagesOn, initializedFPGA, mountedFPGA, statusFPGA]);
 };
 
-// Internal functions
+// Internal functions for the transitions of the Finite State Machine
 
 // Executes the next callback
 nextCallback = function (res, callbackList) {
@@ -44,7 +44,7 @@ hugePagesOn = function (res, callbackList) {
 // FPGA initialized checker
 initializedFPGA = function (res, callbackList) {
   var status_json = 'status_2_init_off';
-  // 0 if huge pages is active, 1 otherwise
+  // 0 if fpga initialized, 1 otherwise
   var code_script = scripts.exec('lspci | grep 19aa:e004');
   code_script.on('exit', function (code) {
     if (code != 0) {
@@ -55,7 +55,47 @@ initializedFPGA = function (res, callbackList) {
   });
 };
 
-// Test function
-lastTest = function (res, callbackList) {
-  common.sendJSON('status_test', res, 200);
-}
+// FPGA mounted checker
+mountedFPGA = function (res, callbackList) {
+  var status_json = 'status_3_mount_off';
+  // 0 if fpga is mounted, 1 otherwise
+  var code_script = scripts.exec('lsmod | grep nfp_driver');
+  code_script.on('exit', function (code) {
+    if (code != 0) {
+      common.sendJSON(status_json, res, 200);
+      return;
+    }
+    nextCallback(res, callbackList);
+  });
+};
+
+// Status of the FPGA (after being mounted)
+statusFPGA = function (res, callbackList) {
+  var code_script = scripts.exec('cat /proc/nfp/nfp_report', function (error, stdout, stderr) {
+    if (error) {
+      console.error(stderr);
+      common.sendJSON('status_3_mount_off', res, 200);
+      return;
+    }
+    common.readJSON('status_4_fpga_ok', function (ans) {
+      // Set the type (player/recorder)
+      if (stdout.indexOf('PLA') != -1) {
+        ans.type = 'player';
+      } else if (stdout.indexOf('REC') != -1) {
+        ans.type = 'recorder';
+      } else {
+        console.error('Neither a player nor recorder');
+        common.sendJSON('status_3_mount_off', res, 200);
+        return;
+      }
+
+      // Set the code (running, paused, stop, etc.)
+      // TODO: Ask type of codes, what they mean
+
+      // Send the response
+      res.status(200).json(ans);
+    });
+  });
+};
+
+// Other Internal Functions
