2397ea728a5f3a62d0012e83911a65041a812b03
JSidrach <juan.sidrach@gmail.com>
2015-02-24 10:18:32 +0100

    Add config file

---
+++ fpga_api/config.js
@@ -0,0 +1,9 @@
+// Configuration file
+var config = {}
+
+// Server port
+config.PORT = 1337;
+// Captures dir (end it with /)
+config.CAPTURES_DIR = 'data/';
+
+module.exports = config;
--- fpga_api/routes/manager.js
+++ fpga_api/routes/manager.js
@@ -1,5 +1,6 @@
 // Manager module
 
 // Package dependencies
-var scripts    = require('child_process');
-var common     = require('./_common.js');
+var scripts = require('child_process');
+var config = require('../config.js');
+var common = require('./_common.js');
