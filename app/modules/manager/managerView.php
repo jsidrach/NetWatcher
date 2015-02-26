a8acef35aa76a1ef853ac154c3d763c6ea5f347e
JSidrach <juan.sidrach@gmail.com>
2015-02-26 13:26:33 +0100

    Minor changes to the manager page

--- app/modules/manager/managerView.php
+++ app/modules/manager/managerView.php
@@ -38,35 +38,84 @@ class managerView extends Common\appView
      * @see \App\Common\Views\appView::renderContent()
      */
     protected function renderContent()
-    { }
-    
+    {}
+
     /**
      * Renders a connection error panel
      */
-    public function renderError() {
-        $this->pLine('<div id="connectionError" class="bootstrap-growl alert alert-danger"
-                           style="position: fixed; margin: 0px 0px 0px -175px; z-index: 9999; top: 60px; width: 350px; left: 50%;">');
-        $this->pLine('<p>', 1);
-        $this->pLine(_('Connection Error. Trying to refresh in') . ' <label id="connectionErrorCountdown"></label>...', 1);
-        $this->pLine('</p>', -1);
-        $this->pLine('<p>');
-        $this->pLine(sprintf(_('Consider visiting the %ssettings%s and %sstatus%s pages'), '<a href="settings">', '</a>', '<a href="status">', '</a>'), 1);
-        $this->pLine('</p>', -1);
-        $this->pLine('</div>', -1);
+    public function renderError()
+    {
+        $this->pLine('<div class="row">');
+        $this->pLine('<div class="col-md-8 col-md-offset-2">', 1);
+        $this->pLine('<div class="alert alert-danger" role="alert" id="connectionError">', 1);
+        $this->pLine('<strong>' . _('Connection Error') . '</strong>. ' . sprintf(_('Consider visiting the %ssettings%s and %sstatus%s pages'), '<a href="settings" class="alert-link">', '</a>', '<a href="status" class="alert-link">', '</a>') . '.', 1);
+        $this->pLine(_('Trying to refresh in') . ' <label id="connectionErrorCountdown"></label>...', 0);
+        $this->pLine('</div>', - 1);
+        $this->pLine('</div>', - 1);
+        $this->pLine('</div>', - 1);
     }
-    
+
     /**
      * Renders an error with HugePages
      */
-    public function renderErrorHP() {
-        //TODO
+    public function renderErrorHP()
+    {
+        /* Error message */
+        $this->pLine('<div class="row">');
+        $this->pLine('<div class="col-md-10 col-md-offset-1">', 1);
+        $this->pLine('<div class="alert alert-danger" role="alert" id="hugePagesOff">', 1);
+        $this->pLine('<p><strong>' . _('Error') . '</strong>: ' . _('HugePages is off and it is required by the FPGA') . '.</p>', 1);
+        $this->pLine('<p>');
+        $this->pLine('<a href="#" data-toggle="modal" data-target="#confirmRebootModal" class="alert-link">', 1);
+        $this->pLine(_('Click here to reboot the FPGA server'), 1);
+        $this->pLine('</a>', - 1);
+        $this->pLine(' ' . _('(it will fix the issue only if the remote server has HugePages as its default boot option)') . '.');
+        $this->pLine('</p>', - 1);
+        $this->pLine('</div>', - 1);
+        $this->pLine('</div>', - 1);
+        $this->pLine('</div>', - 1);
+        /* Reboot modal */
+        $this->pLine('<!-- Reboot confirmation -->');
+        $this->pLine('<div id="confirmRebootModal" class="modal fade" tabindex="-2" role="dialog" aria-hidden="true">');
+        $this->pLine('<div class="modal-dialog">', 1);
+        $this->pLine('<div class="modal-content">', 1);
+        $this->pLine('<div class="modal-body text-justify">', 1);
+        $this->pLine('<p>' . _('The remote server will be rebooted, and all its process stopped') . '.</p>', 1);
+        $this->pLine('<p>' . _('Are you sure you want to reboot?') . '</p>');
+        $this->pLine('</div>', - 1);
+        $this->pLine('<div class="modal-footer">');
+        $this->pLine('<button type="button" data-dismiss="modal" class="btn btn-danger" id="confirmReboot" data-toggle="modal" data-target="#rebootingModal">' . _('Reboot') . '</button>', 1);
+        $this->pLine('<button type="button" data-dismiss="modal" class="btn btn-default">' . _('Cancel') . '</button>');
+        $this->pLine('</div>', - 1);
+        $this->pLine('</div>', - 1);
+        $this->pLine('</div>', - 1);
+        $this->pLine('</div>', - 1);
+        /* Rebooting modal */
+        $this->pLine('<!-- Reboot confirmation -->');
+        $this->pLine('<div id="rebootingModal" data-backdrop="static" data-keyboard="false" class="modal fade" tabindex="-2" role="dialog" aria-hidden="true">');
+        $this->pLine('<div class="modal-dialog">', 1);
+        $this->pLine('<div class="modal-content">', 1);
+        $this->pLine('<div class="modal-header panel-primary">', 1);
+        $this->pLine('<h4 class="modal-title">' . _('Rebooting Web Service') . '</h4>', 1);
+        $this->pLine('</div>', - 1);
+        $this->pLine('<div class="modal-body text-justify">');
+        $this->pLine('<div class="progress">', 1);
+        $this->pLine('<div id="" class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" style="width: 100%">', 1);
+        $this->pLine('<span id="">' . _('Loading') . '</span>', 1);
+        $this->pLine('</div>', - 1);
+        $this->pLine('</div>', - 1);
+        $this->pLine('</div>', - 1);
+        $this->pLine('</div>', - 1);
+        $this->pLine('</div>', - 1);
+        $this->pLine('</div>', - 1);
     }
-    
+
     /**
      * Renders mode selection (player/recorder)
      */
-    public function renderModeSelection() {
-        //TODO
+    public function renderModeSelection()
+    {
+        // TODO
     }
 }
 ?>
