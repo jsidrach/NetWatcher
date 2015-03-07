276c6fbee7d4b195668c4a8ffcf2eec96bafcffc
JSidrach <juan.sidrach@gmail.com>
2015-03-08 00:23:11 +0100

    Major refactor of the io.js web service

---
+++ fpga_api/routes/manager_utils.js
@@ -0,0 +1,108 @@
+// Manager module auxiliary and internal functions
+
+// Package dependencies
+var scripts = require('child_process');
+var config = require('../config.js');
+var common = require('./_common.js');
+var captures_utils = require('./captures_utils.js');
+var statistics_utils = require('./statistics_utils.js');
+
+// Constants
+var playerBitStream = 'bin/nf10g_player.bit';
+var recorderBitStream = 'bin/nf10g_recorder.bit';
+
+
+// Module exports
+
+// Commands
+var rebootCommand = 'sudo /sbin/reboot';
+exports.rebootCommand = rebootCommand;
+
+// Checks the state and programs and mounts the FPGA as a player or recorder
+function initFPGA(req, res, recorder) {
+  var bitstream = recorder ? recorderBitStream : playerBitStream;
+  // Prequisite: HugePages on
+  // 0 if huge pages is not active, 1 if hugepages is active
+  scripts.exec(statistics_utils.checkHugePagesOff).on('exit', function (code) {
+    if (code == 0) {
+      // HugePages off: invalid state to initialize the FPGA
+      common.readJSON('fpga_invalid_state', function (ans) {
+        ans.description = 'Invalid State. HugePages needs to be active to initialize the FPGA.';
+        res.status(412).json(ans);
+      });
+      return;
+    }
+    // HugePages on: initialize the FPGA
+    initBitstream(req, res, bitstream);
+  });
+};
+exports.initFPGA = initFPGA;
+
+// Mounts the FPGA
+function installFPGA(req, res, recorder) {
+  // Check if the FPGA is programmed
+  scripts.exec(statistics_utils.checkInitFPGAOn).on('exit', function (code) {
+    if (code != 0) {
+      // FPGA not programmed: invalid state to mount the FPGA
+      common.readJSON('fpga_invalid_state', function (ans) {
+        ans.description = 'Invalid State. The FPGA must be programmed before mounted.';
+        res.status(412).json(ans);
+      });
+      return;
+    }
+    // Mount the FPGA
+    scripts.exec('sudo ./bin/installDriver.sh').on('exit', function (code) {
+      if (code != 0) {
+        // Internal Error
+        common.logError('Error mounting the FPGA');
+        res.sendStatus(500);
+        return;
+      }
+      if(recorder) {
+        scripts.exec('sudo ./bin/configureAel').on('exit', function (code) {
+          if (code != 0) {
+            // Internal Error
+            common.logError('Error configuring the FPGA');
+            res.sendStatus(500);
+            return;
+          }
+          // Mounted and configured ok
+          common.sendJSON('fpga_mount_ok', res, 200);
+        });
+      } else {
+        // Mounted ok
+        common.sendJSON('fpga_mount_ok', res, 200);
+      }
+    });
+  });
+};
+exports.installFPGA = installFPGA;
+
+
+
+// Internal functions
+
+// Programs and mounts the FPGA with a bitstream
+function initBitstream(req, res, bitstream) {
+  // Program the FPGA
+  scripts.exec('sudo ./bin/impact.sh ' + bitstream).on('exit', function (code) {
+    // Check if the FPGA Is programmed
+    if (code != 0) {
+      // Internal error
+      common.logError('Error initializing the FPGA');
+      res.sendStatus(500);
+      return;
+    }
+    // Reboot the system
+    scripts.exec(rebootCommand, function (error, stdout, stderr) {
+      if (error) {
+        // Internal error
+        common.logError(stderr);
+        res.sendStatus(500);
+        return;
+      }
+      // Programmed ok
+      common.sendJSON('fpga_init_ok', res, 200);
+    });
+  });
+};
--- fpga_api/routes/statistics.js
+++ fpga_api/routes/statistics.js
@@ -1,19 +1,12 @@
 // Real Time Statistics module
 
 // Package dependencies
-var scripts = require('child_process');
-var fs = require('fs');
-var path = require('path');
 var config = require('../config.js');
 var common = require('./_common.js');
+var statistics_utils = require('./statistics_utils.js');
 
-// Constant execs
-var checkHugePagesOff = 'cat /proc/meminfo | grep "HugePages_Total:       0"';
-exports.checkHugePagesOff = checkHugePagesOff;
-var checkInitFPGAOn = 'lspci | grep 19aa:e004';
-exports.checkInitFPGAOn = checkInitFPGAOn;
-var checkFPGAMountedOn = 'lsmod | grep nfp_driver';
-exports.checkFPGAMountedOn = checkFPGAMountedOn;
+
+// Routes
 
 // /ping
 // Simple ping
@@ -30,7 +23,7 @@ exports.delay = function (req, res) {
   } else {
     common.readJSON('statistics_delay', function (ans) {
       ans.delay = delay;
-      ans.maxDelay = config.MAX_DELAY
+      ans.maxDelay = config.MAX_DELAY;
       res.status(200).json(ans);
     });
   }
@@ -40,140 +33,11 @@ exports.delay = function (req, res) {
 // Status of the FPGA
 exports.status = function (req, res) {
   // Finite State Machine. Every transition to a new status (state) is checked with a callback test function
-  nextCallback(res, [hugePagesOn, initializedFPGA, mountedFPGA, statusFPGA]);
-};
-
-// Internal functions for the transitions of the Finite State Machine
-
-// Executes the next callback
-nextCallback = function (res, callbackList) {
-  if (callbackList[0] instanceof Function) {
-    callbackList[0](res, callbackList.slice(1));
-  }
-};
-
-// HugePages active
-hugePagesOn = function (res, callbackList) {
-  var status_json = 'status_1_hugepages_off';
-  // 0 if huge pages is not active, 1 if hugepages is active
-  scripts.exec(checkHugePagesOff).on('exit', function (code) {
-    if (code == 0) {
-      common.sendJSON(status_json, res, 200);
-      return;
-    }
-    nextCallback(res, callbackList);
-  });
-};
-
-// FPGA initialized checker
-initializedFPGA = function (res, callbackList) {
-  var status_json = 'status_2_init_off';
-  // 0 if fpga initialized, 1 otherwise
-  scripts.exec(checkInitFPGAOn).on('exit', function (code) {
-    if (code != 0) {
-      common.sendJSON(status_json, res, 200);
-      return;
-    }
-    nextCallback(res, callbackList);
-  });
-};
-
-// FPGA mounted checker
-mountedFPGA = function (res, callbackList) {
-  var status_json = 'status_3_mount_off';
-  // 0 if fpga is mounted, 1 otherwise
-  scripts.exec(checkFPGAMountedOn).on('exit', function (code) {
-    if (code != 0) {
-      common.sendJSON(status_json, res, 200);
-      return;
-    }
-    nextCallback(res, callbackList);
-  });
-};
-
-// Status of the FPGA (after being mounted)
-statusFPGA = function (res, callbackList) {
-  modeFPGA(function (ans) {
-    // Set the type (player/recorder)
-    if(ans == 'recorder') {
-      runningFPGA(true, function(isRunning) {
-        if(isRunning) {
-          sendDataRecording(res);
-        } else {
-          common.sendJSON('status_4_1_recorder_ready', res, 200);
-        }
-      });
-    } else if(ans == 'player') {
-      runningFPGA(false, function(isRunning) {
-        if(isRunning) {
-          common.sendJSON('status_4_2_playing', res, 200);
-        } else {
-          common.sendJSON('status_4_1_player_ready', res, 200);
-        }
-      });
-    } else {
-      common.sendJSON('status_3_mount_off', res, 200);      
-    }
-  });
-};
-
-// Internal+External Functions
-
-// Gets the mode of the FPGA (player/recorder/error)
-function modeFPGA(callback) {
-  scripts.exec('cat /proc/nfp/nfp_report | tail -n 1', function (error, stdout, stderr) {
-    var ans;
-    if (error) {
-      ans = 'error';
-      common.logError(stderr);
-    }
-    // Set the type (player/recorder)
-    else if (stdout.indexOf('PLA') != -1) {
-      ans = 'player';
-    } else if (stdout.indexOf('REC') != -1) {
-      ans = 'recorder';
-    } else {
-      ans = 'error';
-    }
-    callback(ans);
-  });
-};
-exports.modeFPGA = modeFPGA;
-
-// Gets a boolean value that represents if the FPGA is running (true: yes, false: no)
-function runningFPGA(recorder, callback) {
-  var command = recorder ? 'pgrep launchRecorder || pgrep card2host' : 'pgrep launchPlayer || pgrep host2card';
-  scripts.exec(command).on('exit', function(code) {
-    callback(code == 0);
-  });
-};
-exports.runningFPGA = runningFPGA;
-
-// Other Internal FUnctions
-
-// Sends the current info of the record in progress
-function sendDataRecording(res) {
-  common.readJSON('status_4_2_recording', function (ans) {
-    scripts.exec('ps -eo etime,command | grep launchRecorder.sh | grep -v grep | head -n1', function (error, stdout, stderr) {
-      if (error) {
-        // Internal error
-        common.logError(stderr);
-        res.sendStatus(500);
-        return;
-      }    
-      // Output format:
-      //    [etime] sudo -b nohup ./bin/launchRecorder.sh port bytes simple
-      var parts = stdout.match(/\S+/g);
-      if(parts.length >= 8) {
-        ans.elapsed_time = common.etime2seconds(parts[0]);
-        ans.bytes_total = parseInt(parts[6]);
-        ans.port = parseInt(parts[5]);
-        var capturePath = parts.slice(7).join(' ');
-        var captureStats = fs.statSync(capturePath);
-        ans.bytes_captured = captureStats['size'];
-        ans.capture = path.basename(capturePath);
-      }
-      res.status(200).json(ans);
-    });
-  });
+  statistics_utils.nextCallback(res,
+    [
+      statistics_utils.hugePagesOn,
+      statistics_utils.initializedFPGA,
+      statistics_utils.mountedFPGA,
+      statistics_utils.statusFPGA
+    ]);
 };
