51fdc6c35cf18ff4a0a7c4a673b6f829c9e56083
JSidrach <juan.sidrach@gmail.com>
2015-03-15 21:37:56 +0100

    Refactored the rest of the jquery modules

--- public/js/netwatcher-settings.js
+++ public/js/netwatcher-settings.js
@@ -1,51 +1,74 @@
 // Sets the events
 $(document).ready(function () {
-  $('#serverIp').on('input', function () {
-    checkIp();
-  });
-  checkIp();
+  Settings.init();
 });
 
-// Checks the ip
-function checkIp() {
-  var pingURL = $('#serverIp').val() + '/info/ping';
-  // Get the new data
-  $.ajax({
-    type: 'GET',
-    url: pingURL,
-    dataType: 'jsonp',
-    data: '',
-    timeout: '500',
-    success: function (resp) {
-      setServerIpInput(true);
-    },
-    error: function (e) {
-      setServerIpInput(false);
-    }
-  });
-}
+//
+// Settings
+//
+(function( Settings, $, undefined ) {
 
-// Sets the input to error or success
-function setServerIpInput(value) {
-  var serverIpForm = $('#serverIpForm');
-  var serverIpIcon = $('#ipIcon');
-  if (value) {
-    if (serverIpForm.hasClass('has-warning')) {
-      serverIpForm.removeClass('has-warning');
+  // Internal variables
+  // Server IP
+  var serverIp;
+  // Server IP form
+  var serverIpForm;
+  // Server IP icon
+  var serverIpIcon;
+
+  // Initializes the module
+  Settings.init = function() {
+    // Set the internal variables
+    serverIp = $('#serverIp');
+    serverIpForm = $('#serverIpForm');
+    serverIpIcon = $('#ipIcon');
+    // Set the events
+    serverIp.on('input', function () {
+      checkIp();
+    });
+    checkIp();
+  };
+
+  // Checks the ip
+  function checkIp() {
+    var pingURL = serverIp.val() + '/info/ping';
+    // Get the new data
+    $.ajax({
+      type: 'GET',
+      url: pingURL,
+      dataType: 'jsonp',
+      data: '',
+      timeout: '500',
+      success: function (resp) {
+        setServerIpInput(true);
+      },
+      error: function (e) {
+        setServerIpInput(false);
+      }
+    });
+  };
+
+  // Sets the input to error or success
+  function setServerIpInput(value) {
+    if (value) {
+      if (serverIpForm.hasClass('has-warning')) {
+        serverIpForm.removeClass('has-warning');
+      }
+      serverIpForm.addClass('has-success');
+      if (serverIpIcon.hasClass('glyphicon-warning-sign')) {
+        serverIpIcon.removeClass('glyphicon-warning-sign');
+      }
+      serverIpIcon.addClass('glyphicon-ok');
+    } else {
+      if (serverIpForm.hasClass('has-success')) {
+        serverIpForm.removeClass('has-success');
+      }
+      serverIpForm.addClass('has-warning');
+      if (serverIpIcon.hasClass('glyphicon-ok')) {
+        serverIpIcon.removeClass('glyphicon-ok');
+      }
+      serverIpIcon.addClass('glyphicon-warning-sign');
     }
-    serverIpForm.addClass('has-success');
-    if (serverIpIcon.hasClass('glyphicon-warning-sign')) {
-      serverIpIcon.removeClass('glyphicon-warning-sign');
-    }
-    serverIpIcon.addClass('glyphicon-ok');
-  } else {
-    if (serverIpForm.hasClass('has-success')) {
-      serverIpForm.removeClass('has-success');
-    }
-    serverIpForm.addClass('has-warning');
-    if (serverIpIcon.hasClass('glyphicon-ok')) {
-      serverIpIcon.removeClass('glyphicon-ok');
-    }
-    serverIpIcon.addClass('glyphicon-warning-sign');
-  }
-};
+  };
+
+}( window.Settings = window.Settings || {}, jQuery ));
