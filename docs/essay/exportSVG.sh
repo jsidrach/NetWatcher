e129dd561f9f2d1e7cc98eda473f963ecba18e48
JSidrach <juan.sidrach@gmail.com>
2015-05-26 13:15:43 +0200

    Change png for pdf (vectorial graphics)

---
+++ docs/essay/exportSVG.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# For every svg file
+find ./graphics -name "*.svg" -print0 | while IFS= read -r -d $'\0' line; do
+    # Remove previous pdf
+    rm -f "${line%.*}".pdf
+    # Export svg to pdf
+    inkscape -z -C --file="$line" --export-pdf="${line%.*}".pdf
+    # Remove png
+    rm -f "${line%.*}".png
+done
